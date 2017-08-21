de2958a64a75f3cb142e6f92bbbc97eaac428a45
Phillip Bailey <p0bailey@users.noreply.github.com>
2017-08-21 14:53:43 +0100

    curl software-properties-common

--- ubuntu-16.04/scripts/provision.sh
+++ ubuntu-16.04/scripts/provision.sh
@@ -2,7 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get -y install python-pip python-dev build-essential libffi-dev libssl-dev git
+sudo apt-get -y install python-pip python-dev build-essential libffi-dev libssl-dev git curl software-properties-common
 sudo pip install --upgrade pip
 sudo pip install ansible
 sudo pip install --upgrade setuptools
