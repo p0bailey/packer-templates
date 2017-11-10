5ee1b962b5131db3e553bb0f56131aef954fb3ef
p0bailey <phillip@bailey.st>
2017-11-10 05:25:52 +0000

    Fix: Virtualbox guest addition

--- centos-7.3/scripts/virtualbox.sh
+++ centos-7.3/scripts/virtualbox.sh
@@ -3,6 +3,10 @@ VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 
 # required for VirtualBox 4.3.26
 yum install -y bzip2
+#
+
+yum install -y dkms binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers
+yum install -y kernel-devel
 
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
