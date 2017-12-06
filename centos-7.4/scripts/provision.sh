15d84c2a86428120513d410729ff7dcd93d37c5c
p0bailey <phillip@bailey.st>
2017-12-06 20:07:50 +0000

    Centos 7.4

---
+++ centos-7.4/scripts/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -eux
+sudo yum install -y python-pip python-devel build-essential libffi-dev libssl-dev git openssl-devel  libffi-devel
+sudo pip install --upgrade setuptools
+sudo pip install ansible
+
+sudo yum -y upgrade
---
+++ centos-7.4/scripts/virtualbox.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -eux
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+
+# required for VirtualBox 4.3.26
+yum install -y bzip2
+
+
+yum install -y dkms binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers
+yum install -y kernel-devel
+
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
