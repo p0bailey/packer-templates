a166ae3b960c7d8e58e09c802775e7a7878b496e
p0bailey <phillip@bailey.st>
2018-01-05 20:51:17 +0000

    Added debian 9

---
+++ debian-9/scripts/provision.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get -y install python-pip python-dev git build-essential libffi-dev libssl-dev
+sudo pip install --upgrade cffi
+sudo pip install ansible
+sudo pip install --upgrade setuptools
