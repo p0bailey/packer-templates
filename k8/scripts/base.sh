dc4b5def4e7d9432bfa0225e0ec636121acfcae2
p0bailey <phillip@bailey.st>
2018-11-01 05:49:27 +0000

    k8 switched to Debian9

--- k8/scripts/base.sh
+++ k8/scripts/base.sh
@@ -4,7 +4,11 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential dkms
 
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
-sed -i -e 's/%sudo\s*ALL=(ALL:ALL) ALL/%sudo\tALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers;
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
+sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
+rm -f /etc/udev/rules.d/70-persistent-net.rules;
+mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
