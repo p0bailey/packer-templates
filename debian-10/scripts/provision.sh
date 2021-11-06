708620532e4f5b5e61a4cd7b9928814830703770
p0bailey <phillip@bailey.st>
2021-11-06 16:38:18 +0000

    Sat  6 Nov 2021 16:38:18 GMT

--- debian-10/scripts/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get dist-upgrade -y
-apt-get -y install vim python-pip python3-venv python3-pip unzip git
-pip3 install ansible molecule
